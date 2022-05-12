019e98e32bd3f6d25a196c33e63a860f0981f16d
Ryan Brisch <brischster@gmail.com>
2022-05-12 10:02:22 -0600

    created render questions function and render scoreboard function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-var questionContent = $(".questionContent");
+var questionContent = $("#questionText");
 var gameText = $("#game-timer");
 var timerEl = $("#game-timer");
 var startBtn = $("#start-btn");
@@ -6,126 +6,75 @@ var submitBtn = $("#submit-btn");
 var answerOne = $("#answer-1");
 var answerTwo = $("#answer-2");
 var answerThree = $("#answer-3");
-var answerFour = $("#answer-4");
 var listOfScores = $("#highscores");
 
 var questionsArray = [
   {
-    title: "Question #1:",
     question: "A true Bitcoin psycho has what type of hands?",
-    isCorrect: true,
+    answers: {
+      a: "Paper",
+      b: "Diamond",
+      c: "Soft",
+    },
+
+    correctAnswer: "b",
   },
   {
-    title: "Question #2:",
     question: "What is the best way to lose your Bitcoin holdings?",
-    isCorrect: true,
+    answers: {
+      a: "Staking",
+      b: "Confiscated by the Canadian Govt.",
+      c: "Boating Accident",
+    },
+    correctAnswer: "c",
   },
   {
-    title: "Question #3:",
     question: "How much is 1 Bitcoin worth?",
-    isCorrect: true,
+    answers: {
+      a: "$32,000",
+      b: "1 Bitcoin",
+      c: "1 Gold Bar",
+    },
+    correctAnswer: "b",
   },
   {
-    title: "Question #4:",
     question: "Tick. Tock. Next. ____?",
-    isCorrect: true,
+    answers: {
+      a: "Block",
+      b: "Question",
+      c: "Pump",
+    },
+    correctAnswer: "a",
   },
   {
-    title: "Question #5:",
     question: "What is the first rule of Bitcoin?",
-    isCorrect: true,
+    answers: {
+      a: "Keep your Bitcoin on an exchange",
+      b: "Once it hits 100k sell it for USD",
+      c: "Just Hodl",
+    },
+    correctAnswer: "c",
   },
 ];
 
-var correctAnswerArr = [
-  {
-    title: "Question 1:",
-    answer: "Paper",
-    iscorrect: false,
-  },
-  {
-    title: "Question 1:",
-    answer: "Diamond",
-    iscorrect: true,
-  },
-  {
-    title: "Question 1:",
-    answer: "Soft",
-    iscorrect: false,
-  },
-  {
-    title: "Question 2:",
-    answer: "Staking",
-    iscorrect: false,
-  },
-  {
-    title: "Question 2:",
-    answer: "Confiscated by the Canadian Govt",
-    iscorrect: false,
-  },
-  {
-    title: "Question 1:",
-    answer: "Boating Accident",
-    iscorrect: true,
-  },
-  {
-    title: "Question 3:",
-    answer: "$32,000 USD",
-    iscorrect: false,
-  },
-  {
-    title: "Question 3:",
-    answer: "1 Bitcoin",
-    iscorrect: true,
-  },
-  {
-    title: "Question 3:",
-    answer: "1 Gold Bar",
-    iscorrect: false,
-  },
-  {
-    title: "Question 4:",
-    answer: "Block",
-    iscorrect: false,
-  },
-  {
-    title: "Question 4:",
-    answer: "Question",
-    iscorrect: false,
-  },
-  {
-    title: "Question 4:",
-    answer: "Pump",
-    iscorrect: false,
-  },
-  {
-    title: "Question 5:",
-    answer: "Keep your Bitcoin on an exchange",
-    iscorrect: false,
-  },
-  {
-    title: "Question 5:",
-    answer: "Once it hits 100k sell it for USD",
-    iscorrect: false,
-  },
-  {
-    title: "Question 5:",
-    answer: "You never sell your Bitcoin",
-    iscorrect: false,
-  },
-];
+console.log(questionsArray[0].answers.a);
 
 // Game Logic Variables (Timer, Index Numbers
 var timer = 30;
 var currentIndex = 0;
 
+init();
+
 // Actions upon loading page
-
-// hide game button
-// show start button
-// render high scores
-// render first question
-
+function init(params) {
+  // hide game button
+  submitBtn.hide();
+  // show start button
+  // render high scores
+  createScoreboard();
+  // render first question
+  createQuestion();
+}
 // create event listeners for buttons
 // Start game btn
 // submit answer btn
@@ -145,10 +94,32 @@ var currentIndex = 0;
 // re-render high scores
 
 // render highscores:
+function createScoreboard(params) {
+  var currentScore = JSON.parse(localStorage.getItem("psychoScore")) || [];
+
+  listOfScores.empty();
+  if (listOfScores.length === 0) {
+    return listOfScores.text("No One has Played Yet!");
+  }
+  for (var i = 0; i < currentScore.length; i++) {
+    var scoreObj = currentScore[i];
+    var newScore = $("<li>", {
+      class: "list-group-item",
+    }).text(scoreObj.initials + "----" + scoreObj.score);
+
+    listOfScores.append(newScore);
+  }
+}
 // get current local storage data
 // clear out current highscore field
 // loop through array of score objects
 // render new li for each score obj
 
 // render questions and answers
+function createQuestion() {
+  questionContent.text(questionsArray[currentIndex].question);
+  answerOne.text(questionsArray[currentIndex].answers.a);
+  answerTwo.text(questionsArray[currentIndex].answers.b);
+  answerThree.text(questionsArray[currentIndex].answers.c);
+}
 // update forms and question
